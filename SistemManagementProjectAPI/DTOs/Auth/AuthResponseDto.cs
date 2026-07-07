4a298c6ece51e10bfa5d6c62ddd400612c2d214c
MUHAMMAD MALFIN <malvin.arkade7@gmail.com>
2026-07-07 14:55:02 +0700

    Add DTO Auth

---
+++ SistemManagementProjectAPI/DTOs/Auth/AuthResponseDto.cs
@@ -0,0 +1,8 @@
+namespace SistemManagementProjectAPI.DTOs.Auth;
+
+public class AuthResponseDto
+{
+    public string Token { get; set; } = string.Empty;
+    public DateTime Expires { get; set; }
+    public UserDto User { get; set; } = null!;
+}
